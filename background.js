506db251c03a347dcfcf86919a2c375f9669ddb9
usman <usman12k2@gmail.com>
2023-07-17 21:24:53 +0400

    Fixed the issue of the icon not loading sometimes

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
-let tabs_already_injected_list = []
-
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.url.startsWith("https://www.youtube.com/watch") && tabs_already_injected_list.includes(!tabId)) {
-	  tabs_already_injected_list.push(tabId)
-    chrome.tabs.executeScript(tabId, { file: "injected.js" });
-  }
-});
-
 chrome.runtime.onMessage.addListener(function(link){
     chrome.tabs.create({ url: link })
 })
--- injected.js
+++ injected.js
@@ -1,41 +1,73 @@
-// UI
-
-console.log("Youtube Bookmars Injected")
 
 let youtube_block_where_the_widget_will_be_shown = document.querySelector(".ytp-left-controls")
 
-let widget = document.createElement("div")
-widget.style = 'padding:5px'
-widget.innerHTML = `<img src="${chrome.runtime.getURL("Icons/plus.svg")}"
-  style="width: 100%; height: 100%; cursor: pointer;"
-  class="ytp-chapter-title ytp-button ytp-chapter-container-disabled"
-  title="Add Bookmark">`;
+if (!youtube_block_where_the_widget_will_be_shown){ // Check if this element exists
+    AddMutationObserver() // start observing for that element 
+} else{
+    main()
+}
+
+function main(){
+    // add the plus icon
+    let widget = document.createElement("div")
+    widget.style = 'padding:5px'
+    widget.innerHTML = `<img src="${chrome.runtime.getURL("Icons/plus.svg")}"
+    style="width: 100%; height: 100%; cursor: pointer;"
+    class="ytp-chapter-title ytp-button ytp-chapter-container-disabled"
+    title="Add Bookmark">`;
 
 
-youtube_block_where_the_widget_will_be_shown.appendChild(widget)
+    youtube_block_where_the_widget_will_be_shown.appendChild(widget)
 
 
-// Listener
+    // Listeners
 
-widget.addEventListener("click",function(){
-    alert("Youtube Bookmarker: Bookmark Added")
-    let returnVal = ExtractYoutubeVidInfo()
+    widget.addEventListener("click",function(){
+        alert("Youtube Bookmarker: Bookmark Added")
+        let returnVal = ExtractYoutubeVidInfo()
 
-    let unique_id = returnVal[0]
-    let object = returnVal[1]
+        let unique_id = returnVal[0]
+        let object = returnVal[1]
 
-    UpdateDataInChrome(unique_key = unique_id, object = object)
-})
+        UpdateDataInChrome(unique_key = unique_id, object = object)
+    })
+
+}
+
+
+function AddMutationObserver(){
+    let observer = new MutationObserver(function(mutations){
+        for (let each_change of mutations){ 
+            if (each_change.addedNodes){ // if the change involves adding of new elements
+
+                for (let each_added_node of each_change.addedNodes){ // check if the added element has this specific class
+                    if (each_added_node.classList && each_added_node.classList.contains("ytp-left-controls")){
+                        youtube_block_where_the_widget_will_be_shown = document.querySelector(".ytp-left-controls") // element found
+                        main()
+                        observer.disconnect()
+                        return
+                    }
+
+                }
+            }
+
+        }
+
+    })
+    observer.observe(document.documentElement,{childList:true, subtree:true})
+}
+
+
 
 
 function ExtractYoutubeVidInfo(){
     let time_extracted = ExtractTime()
 
     let vid_url = new URLSearchParams(window.location.search)
-    let unique_id = vid_url.get("v")
+    let unique_id = vid_url.get("v") // get the "v" parameter of the url
     let list_id
     if (vid_url.has("list")){
-        list_id = vid_url.get("list")
+        list_id = vid_url.get("list") // get the "list" parameter of the url if it exists (videos seen in playlists)
     } else{
         list_id = null
     }
