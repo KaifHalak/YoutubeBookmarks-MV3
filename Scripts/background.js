0e4bd9041316c66b308fa1030c7ce4dfd826ca68
usman <usman12k2@gmail.com>
2023-07-18 15:15:28 +0400

    Finally fixed the issue of the icon not loading sometimes

--- Scripts/background.js
+++ Scripts/background.js
@@ -2,3 +2,25 @@
 chrome.runtime.onMessage.addListener(function(link){
     chrome.tabs.create({ url: link })
 })
+
+let injected_tabs = []
+
+chrome.tabs.onUpdated.addListener(async function(tabId,changeInfo) {
+    // Inject when (1 - Tab is loaded, 2 - Tab as not already been injected by the script)
+    let tab_url = await getTab()
+    if (changeInfo.status === 'complete' && tab_url.startsWith("https://www.youtube.com") && !injected_tabs.includes(tabId)){
+        injected_tabs.push(tabId)
+        chrome.scripting
+        .executeScript({
+          target : {tabId : tabId},
+          files : ["Scripts/injected.js"],
+        }).then(function(){console.log("injected")})
+    }
+  });
+
+
+  async function getTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let tabs = await chrome.tabs.query(queryOptions);
+    return tabs[0].url;
+  }
